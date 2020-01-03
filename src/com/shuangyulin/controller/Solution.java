b45127d26fd8ad34172a9f1ee1385dd6958b84c4
zkc16251101203 <429246016@qq.com>
2020-01-03 11:21:15 +0800

    commit

---
+++ src/com/shuangyulin/controller/Solution.java
@@ -0,0 +1,12 @@
+package com.shuangyulin.controller;
+
+/**
+ * Created in 2020-01-02 12:36.
+ *
+ * @author Zkc
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
